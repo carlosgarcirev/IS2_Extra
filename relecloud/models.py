0f764fabd881effc1e8bf0a7e1a9167edc56155e
carlosgarcirec <carlosgarciarevillas@gmail.com>
2024-06-24 18:13:39 +0200

    Creamos el modelo de las opiniones

--- relecloud/models.py
+++ relecloud/models.py
@@ -52,4 +52,16 @@ class InfoRequest(models.Model):
         on_delete=models.PROTECT
     )
 
+class Opinion(models.Model):
+    title = models.CharField(max_length=200)
+    opinion_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    cruise = models.ForeignKey(
+        'Cruise',
+        on_delete=models.CASCADE,
+        related_name='opinions'
+    )
+
+    def __str__(self):
+        return self.title
 
