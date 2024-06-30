8ace6ef4a05c6ae447b0349dc807a8672e501bed
Alvaro Salvador <7902011@alumnos.ufv.es>
2024-06-30 20:37:04 +0200

    Añado la URL

--- relecloud/urls.py
+++ relecloud/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cruise/<int:pk>', views.CruiseDetailView.as_view(), name='cruise_detail'),
     path('info_request', views.InfoRequestCreate.as_view(), name='info_request'),
     path('opiniones/', views.opiniones, name='opiniones'),
+    path('info_request_success/', views.info_request_success, name='info_request_success'),
 ]
